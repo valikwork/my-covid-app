a63e7feab6d33a81eae927c7342cff76d8cc576d
valikwork <osiptsev.valik@gmail.com>
2021-06-21 17:27:59 +0300

    map added for countries

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,10 @@
-import React, { useEffect } from 'react'
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
-import L from 'leaflet';
+import { MapContainer, TileLayer } from "react-leaflet";
 import IconWithText from './IconWithText';
 
 export default function Map({ lat, long, cases }) {
 
     const defaultPosition = [lat, long]; // Paris position
-    console.log('map');
 
     return (
         <MapContainer className="map__container" center={defaultPosition} zoom={10} scrollWheelZoom={false}>
